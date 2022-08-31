12d91fb2295c678bdc76349c7c5d542057bc1ca9
Rosculet <rousculet89@gmail.com>
2022-08-31 19:10:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList <Integer> list1 = new LinkedList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+
+        LinkedList <Integer> list3 = new LinkedList<>();
+
+    }
+}
